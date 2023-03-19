d1c5983a03397996319312bfda97a811f9bf5a35
norindes <norindes@gmail.com>
2023-03-19 03:11:39 -0600

    footer added

--- main.py
+++ main.py
@@ -6,6 +6,8 @@ from docx.enum.text import WD_BREAK
 from docx.shared import Inches
 from docx.enum.table import WD_TABLE_ALIGNMENT
 from docx.enum.text import WD_PARAGRAPH_ALIGNMENT
+
+from docx.oxml.ns import qn
 import os
 
 def loadxlsx():
@@ -13,43 +15,69 @@ def loadxlsx():
     folder_path = "./xlsx/"
     imgs_path = "./imgs/"
     xlsx_files = glob.glob(folder_path + "*.xlsx")
+    
 
     for xlsx_file in xlsx_files: #loop through all the files
         createdocx(xlsx_file, imgs_path)
 
 def createdocx(xlsx_file, imgs_path):
     """Create the Quickestspecs"""
-
-    df = pd.read_excel(xlsx_file)
-    df = df[df['4RA85F [Product]'] != '##BLANK##']
-
-    img_path = os.path.join(imgs_path, 'c08518669.png')
-    prod_name = df.loc[df['Tag'] == 'prodname', '4RA85F [Product]'].iloc[0]
-
     doc = Document()
+    df = pd.read_excel(xlsx_file) #read the file into a pandas dataframe
+    df = df[df['4RA85F [Product]'] != '##BLANK##'] #remove the ##BLANK##
 
+    prod_name = df.loc[df['Tag'] == 'prodname', '4RA85F [Product]'].iloc[0] #get the product name
+
+    
+    ################################################################ Header
+    
     header = doc.sections[0].header
-    header_table = header.add_table(rows=1, cols=2, width=Inches(8)) 
-    header_table.rows[0].height = Inches(.5)
+    header_table = header.add_table(rows=1, cols=2, width=Inches(8))
+    header_table.columns[0].width = Inches(2)
+    header_table.columns[1].width = Inches(6)
     header_table.alignment = WD_TABLE_ALIGNMENT.CENTER
-    header_table.allow_autofit = False
 
-    cell = header_table.cell(0, 0)
-    cell_paragraph = cell.add_paragraph()
-    cell_paragraph.add_run("Quickestspecs").font.size = Pt(24)
+    header_paragraph = header_table.cell(0, 0).paragraphs[0]
+    header_paragraph.style.font.size = Pt(16)
+    header_paragraph.text = "Quickestspecs"
+    header_paragraph = header_table.cell(0, 1).paragraphs[0]
+    header_paragraph.text = prod_name
 
-    cell = header_table.cell(0, 1)
-    cell_paragraph = cell.add_paragraph()
-    cell_paragraph.add_run(prod_name).font.size = Pt(12)
-    cell.paragraphs[0].alignment = WD_PARAGRAPH_ALIGNMENT.LEFT
+
+    ################################################################ Footer
+
+    footer = doc.sections[0].footer 
+    footer_table = footer.add_table(rows=1, cols=3, width=Inches(8)) 
+
+    footer_table.columns[0].width = Inches(1)
+    footer_table.columns[1].width = Inches(6)
+    footer_table.columns[2].width = Inches(1)
+
+    footer_table.rows[0].height = Inches(.4)
+    footer_table.alignment = WD_TABLE_ALIGNMENT.CENTER
+
+
+
+    footer_paragraph = footer_table.cell(0, 0).paragraphs[0]
+    footer_paragraph.style.font.size = Pt(10)
+    footer_paragraph.add_run().add_picture(imgs_path + "hp-logo.png", width=Inches(.4), height=Inches(.4))
+    footer_paragraph = footer_table.cell(0, 1).paragraphs[0]
+    footer_paragraph.text = "Worldwide — Version 1 — March 19, 2023"
+    footer_paragraph = footer_table.cell(0, 2).paragraphs[0]
+    footer_paragraph.text = "page x"
+    
+
 
 
 
     ################################################################ Callout section
 
-    
+    img_path = os.path.join(imgs_path, 'c08518669.png')
+    img_path2 = os.path.join(imgs_path, 'c08518762.png')
 
-    doc.add_picture(img_path, width=Inches(6.0))
+    doc.add_picture(img_path, width=Inches(6))
+    doc.add_paragraph().add_run().add_break(WD_BREAK.PAGE)
+    doc.add_picture(img_path2, width=Inches(6))
     doc.add_paragraph().add_run().add_break(WD_BREAK.PAGE)
 
     ################################################################ At a glance section
@@ -71,6 +99,8 @@ def createdocx(xlsx_file, imgs_path):
 
     ################################################################ Operating system section
 
+    doc.add_paragraph().add_run("OPERATING SYSTEM").bold = True
+
     ossuppted_values = df.loc[df['Tag'] == 'ossuppted', ['ContainerName', '4RA85F [Product]']].iloc[0]
      
     ossuppted_title = doc.add_heading(ossuppted_values['ContainerName'], level=1)
@@ -80,6 +110,16 @@ def createdocx(xlsx_file, imgs_path):
     ossuppted_subtitle = doc.add_heading(level=2)
     ossuppted_subtitle.add_run(ossuppted_subtitle_replace).bold = False
 
+    doc.add_paragraph().add_run().add_break(WD_BREAK.PAGE)
+
+
+    ################################################################ Dimensions and Weight section
+
+    doc.add_paragraph().add_run("WEIGHTS & DIMENSIONS").bold = True
+
+    dimenus_values = df.loc[df['Tag'] == 'dimenus', ['ContainerName', '4RA85F [Product]']].iloc[0]
+    dimenus_title = doc.add_heading(dimenus_values['ContainerName'], level=1)
+    dimenus_title.style.font.size = Pt(14)
 
             
     doc.save('quickestspecs.docx')
