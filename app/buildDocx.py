b05ac569a174b1bcf44d5f221333a9799c820d5f
jinjernot <norindes@gmail.com>
2024-01-17 21:21:51 -0600

    PDF online

--- app/buildDocx.py
+++ app/buildDocx.py
@@ -1,7 +1,6 @@
 from app.core.tech_specs.tech_specs import tech_specs_section
 from app.core.overview.overview import overview_section
 from app.core.format.format import format_document
-
 from app.core.tables.system_unit import system_unit_section
 from app.core.tables.displays import displays_section
 from app.core.tables.options import options_section
@@ -9,15 +8,12 @@ from app.core.tables.audio import audio_section
 from app.core.tables.fingerprint import fingerprint_section
 from app.core.tables.storage import storage_section
 from app.core.tables.network import network_section
-#from app.core.format.superscript import superscript
-
 import pandas as pd
 from docx import Document
+from zipfile import ZipFile
+from docx2pdf import convert
 
 imgs_path = "./imgs/"
-from zipfile import ZipFile
-import os
-
 
 def createdocx(file):
     """Create the Quickestspecs"""
@@ -37,15 +33,16 @@ def createdocx(file):
     audio_section(doc, file, html_file)
     fingerprint_section(doc, file, html_file)
     options_section(doc, file, html_file)
-    #superscript(doc, file, html_file)
 
     docx_file = 'quickestspecs.docx'
-    pdf_file = 'quickestspecs.pdf'
     doc.save(docx_file)
-    os.rename(docx_file, pdf_file)
+
+    # Convert DOCX to PDF using docx2pdf
+    convert(docx_file)
 
     # Create a zip file and add specific files to it
     zip_file_name = 'quickestspecs.zip'
     with ZipFile(zip_file_name, 'w') as zipf:
         zipf.write(html_file)
-        zipf.write(pdf_file)
+        zipf.write(docx_file)
+        zipf.write(docx_file.replace('.docx', '.pdf'))  # Add the generated PDF file to the zip
